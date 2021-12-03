47cc305402c44b1254608740fca3bd11c9549ad9
camaias <carolinemaiasouza@live.com>
2021-12-03 19:16:29 -0300

    Incluindo funções: exportando dados p/ JSON e validações

--- resolucao.js
+++ resolucao.js
@@ -1,10 +1,14 @@
 var data_obj;
 
 //    LEITURA DO ARQUIVO:
-function ler_arquivo() {
+function ler_arquivo_corrompido() {
     var fs = require('fs');
-    var data = fs.readFileSync('broken-database.json', 'utf8');
-    data_obj = JSON.parse(data);
+    try {
+        var data = fs.readFileSync('broken-database.json', 'utf8');
+        data_obj = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+   }
 }
 
 //    CORREÇÃO NOMES:
@@ -33,9 +37,67 @@ function corrige_quantidade() {
     }
 }
 
+//    EXPORTAR BANCO CORRIGIDO:
+function exporta_arquivo_corrigido() {
+    var fs = require('fs');
+    var data_json = JSON.stringify(data_obj);
+    var dados = fs.writeFileSync('saida.json', data_json,'utf8');
+}
 
+// LEITURA DO ARQUIVO (BANCO CORRIGIDO):
+function ler_arquivo_tratado() {
+    var fs = require('fs');
+    try {
+        var data = fs.readFileSync('saida.json', 'utf8');
+        data_obj = JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-ler_arquivo();
+// VALIDAÇÃO 1: IMPRESSÃO DE NOMES DOS PRODUTOS POR CATEGORIA (EM ORDEM ALFABÉTICA) E ID (EM ORDEM CRESCENTE):
+function validacao_nomes() {    
+    data_obj.sort(function(a, b) {
+        return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;   
+    })
+    data_obj.sort(function(a, b) {
+        return a.category < b.category ? -1 : a.category > b.category ? 1 : 0;
+    })
+    for(var i = 0; i < data_obj.length; i++) {
+    console.log(data_obj[i].name);
+    }
+} 
+
+// VALIDAÇÃO 2: IMPRESSÃO DE VALORES TOTAIS DOS PRODUTOS POR CATEGORIA:
+function validacao_valores() {
+    var valor_final = 0;
+    var categoria_atual = "";
+
+    data_obj.sort(function(a, b) {
+        return a.category < b.category ? -1 : a.category > b.category ? 1 : 0;
+    })
+
+    for(i = 0; i < data_obj.length; i++) {
+        if (categoria_atual != data_obj[i].category) {
+            categoria_atual = data_obj[i].category;
+            for(j = 0; j < data_obj.length; j++) {
+                if (categoria_atual == data_obj[j].category) {
+                    valor_total_item = data_obj[j].quantity * data_obj[j].price;
+                    valor_final = valor_final + valor_total_item;
+                }
+            }
+            console.log(categoria_atual + ": R$ " + valor_final.toFixed(2));            
+        }  
+        valor_final = 0;
+    }
+}
+
+ler_arquivo_corrompido();
 corrige_nome();
 corrige_preco();
 corrige_quantidade();
+exporta_arquivo_corrigido();
+ler_arquivo_tratado();
+validacao_nomes();
+validacao_valores();
+
