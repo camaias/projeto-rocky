8aea85a7d51bc0b1c650f10005f43e4abf796a46
camaias <carolinemaiasouza@live.com>
2021-12-08 14:30:52 -0300

    inclusão de observação nas linhas 39 e 41.

--- resolucao.js
+++ resolucao.js
@@ -36,6 +36,9 @@ function fix_quantity() {
     for (var i = 0; i < data_obj.length; i++) {
         if(typeof(data_obj[i].quantity) == "undefined") {
             data_obj[i] = {'id': data_obj[i].id, 'name': data_obj[i].name, 'quantity': 0, 'price': data_obj[i].price, 'category': data_obj[i].category};
+            //console.log(data_obj[i]); // impressão dos objetos após correção.
+        } else {
+            // console.log(data_obj[i]); //impressão dos objetos após correção.
         }
     }
 }
